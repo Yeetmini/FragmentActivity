887714d154e1b2edba47b19d08a2423b70b7aaf2
Yeetmini <tsucre007@gmail.com>
2024-03-12 12:22:25 -0400

    Added second fragment and altered main activity

--- app/src/main/java/edu/temple/fragment/MainActivity.kt
+++ app/src/main/java/edu/temple/fragment/MainActivity.kt
@@ -10,5 +10,6 @@ class MainActivity : AppCompatActivity() {
 
         supportFragmentManager.beginTransaction()
             .add(R.id.fragmentContainerView2, ColorFragment())
+            .commit()
     }
 }
